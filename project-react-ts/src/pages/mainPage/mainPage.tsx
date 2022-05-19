7342194fd3a716e5b97853166a8465009f67add8
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-19 21:30:49 +0300

    fix: added props in Banner

--- project-react-ts/src/pages/mainPage/mainPage.tsx
+++ project-react-ts/src/pages/mainPage/mainPage.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { Banner } from "../../containers/Banner/Banner";
 import { StickersMain } from "../../containers/StickersMain/StickersMain";
+import { BigButton } from "../../components/common-components/Button";
+import { COLOR } from "../../styled-components/color-constants";
 
 export const MainPage = () => {
     return (
         <>
-            <Banner />
+            <Banner
+                paddingTop = { 142 }
+                paddingBottom = { 142 }
+                backgroundImage = { require("./../../images/Banners/BannerFirstPage.svg") }
+            >
+                <BigButton
+                    paddingTop = { 8 }
+                    paddingRight = { 123 }
+                    paddingBottom = { 8 }
+                    paddingLeft = { 123 }
+                    maxWidth = { 443 }
+                    borderRadius = { 5 }
+                    backGroundColor = { "transparent" }
+                    fontSize = { 36 }
+                    lineHeight = { 49 }
+                    color = { COLOR.metallicBlue }
+                    textShadow = { COLOR.blackOpacityTwentyFive }
+                >
+                    Create your own stickers
+                </ BigButton>
+            </Banner>
             <StickersMain />
         </>
     );
