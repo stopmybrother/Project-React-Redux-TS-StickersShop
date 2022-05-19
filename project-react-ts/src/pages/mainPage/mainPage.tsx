1ca6f38a4af1d7a0496aa8f37069244038270369
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-19 20:49:01 +0300

    feature: added directory pages and included there MainPage (so in index now)

---
+++ project-react-ts/src/pages/mainPage/mainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Banner } from "../../containers/Banner/Banner";
+import { StickersMain } from "../../containers/StickersMain/StickersMain";
+
+export const MainPage = () => {
+    return (
+        <>
+            <Banner />
+            <StickersMain />
+        </>
+    );
+};
