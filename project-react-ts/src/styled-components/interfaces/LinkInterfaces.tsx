655f9a0942828d7a73482d4c3c6d37e0859e6c3f
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-21 00:35:45 +0300

    fix: changed and added Link-interfaces

--- project-react-ts/src/styled-components/interfaces/LinkInterfaces.tsx
+++ project-react-ts/src/styled-components/interfaces/LinkInterfaces.tsx
@@ -4,3 +4,17 @@ export interface ILink {
     width?: number;
     height?: number;
 };
+
+export interface ITextLink {
+    marginTop?: number;
+    marginRight?: number;
+    marginBottom?: number;
+    marginLeft?: number;
+
+    fontSize?: number;
+    fontWeight?: number;
+    lineHeight?: number;
+    color?: string;
+
+    textAlign?: string;
+};
