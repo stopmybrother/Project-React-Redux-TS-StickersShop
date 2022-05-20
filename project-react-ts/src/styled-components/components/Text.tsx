427a40683056bfeb36eceb4700921a40df0b1dad
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-21 00:39:15 +0300

    fix: changed and added Text-component

--- project-react-ts/src/styled-components/components/Text.tsx
+++ project-react-ts/src/styled-components/components/Text.tsx
@@ -7,14 +7,21 @@ export const Text = styled.p<IText>`
   margin-right: ${ p => p.marginRight || 0 }px;
   margin-bottom: ${ p => p.marginBottom || 0 }px;
   margin-left: ${ p => p.marginLeft || 0 }px;
+  
+  padding-top: ${ p => p.paddingTop || 0 }px;
+  padding-right: ${ p => p.paddingRight || 0 }px;
+  padding-bottom: ${ p => p.paddingBottom || 0 }px;
+  padding-left: ${ p => p.paddingLeft || 0 }px;
 
   max-width: 100%;
+  min-width: ${ p => p.minWidth || 0 }px;
   width: 100%;
 
   font-size: ${ p => p.fontSize || 24 }px;
   font-weight: ${ p => p.fontWeight || 600 };
   line-height: ${ p => p.lineHeight || 33 }px;
   
+  background-color: ${ p => p.backgroundColor || "none" };
   color: ${ p => p.color || "none" };
   text-align: ${ p => p.textAlign || "center" };
 `;
