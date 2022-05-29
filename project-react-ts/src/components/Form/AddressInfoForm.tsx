1e74434d899691e3871083c58829408d762935b5
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 21:25:14 +0300

    fix codeStyle in Form/AddressInfoForm.tsx

--- project-react-ts/src/components/Form/AddressInfoForm.tsx
+++ project-react-ts/src/components/Form/AddressInfoForm.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { GeneralWrapper } from "../../styled-components/components/Wrapper";
 import { TextInForm } from "../../styled-components/components/Text";
-import { IInfoInputs, ADDRESS_INFO_INPUTS } from "../../mock-data/FormInputs";
+import { IInfoInputs, ADDRESS_INFO_INPUTS } from "../../constants/FormInputs";
 import { FormInput } from "./FormInput";
 
 export const AddressInfo = () => {
@@ -31,7 +31,6 @@ export const AddressInfo = () => {
                 { ADDRESS_INFO_INPUTS.map( ( input: IInfoInputs ) => (
                     <FormInput
                         key = { input.id }
-                        // value = { personalInfo[input.name] }
                         { ...input }
                         onChange = { HandleChange }
                     />
