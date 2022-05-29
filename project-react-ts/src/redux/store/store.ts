af446f38f93ca0c49d6785ffc45dffc134fbe0af
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 20:50:35 +0300

    feature: created store in redux/store/store.ts

--- project-react-ts/src/redux/store/store.ts
+++ project-react-ts/src/redux/store/store.ts
@@ -1,6 +1,5 @@
-import { createStore, compose } from 'redux';
+import { legacy_createStore as createStore, compose } from 'redux';
 import rootReducer from "../reducers/rootReducer";
-// import { createStore, compose } from "redux";
 
 const composeEnhancers =
     process.env.NODE_ENV !== "production" &&
@@ -8,17 +7,17 @@ const composeEnhancers =
     ( window as any ).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
         ( window as any ).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__( {} ) : compose;
 
-const configureStore = ( preloadedState: any ) => {
+const configureStore = ( preloadedState: any ): any => (
     createStore(
         rootReducer,
         preloadedState,
         composeEnhancers(),
     )
-};
+);
 
 const store = configureStore( {} );
 
 export type Store = typeof store;
-// export type AppDispatch = Store["dispatch"]; // ???
+export type AppDispatch = Store['dispatch'];
 
 export default store;
