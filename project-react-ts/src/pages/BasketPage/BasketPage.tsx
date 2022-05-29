e5b24ba556eceee1cb92dbfebf8d0cfce0a23f8d
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 22:30:48 +0300

    fix: added sorted array of stickers in basket in pages/BasketPage/BasketPage.tsx

--- project-react-ts/src/pages/BasketPage/BasketPage.tsx
+++ project-react-ts/src/pages/BasketPage/BasketPage.tsx
@@ -10,8 +10,10 @@ import { Cross } from "../../components/Cross/Cross";
 import { CounterButtons } from "../../components/Counter/CounterButtons";
 import { CartTotals } from "../../components/CartTotals/CartTotals";
 import { RouterLink } from "../../components/common-components/RouterLink";
+import { IStickers, STICKERS } from "../../constants/stickers";
 
 export const BasketPage = () => {
+    let stickersInBasket = STICKERS.filter((sticker: IStickers) => sticker.inBasket)
     return (
         <>
             <Banner
@@ -35,130 +37,139 @@ export const BasketPage = () => {
                 paddingTop = { 50 }
                 paddingBottom = { 50 }
             >
-                <WrapperGridBasket
-                    rows = { 2 }
-                    rowWidth = { 64 }
-                    justifyContent="center"
-                >
-                    <Text
-                        fontSize = { 20 }
-                        lineHeight = { 27 }
-                        color = { COLOR.charcoalPrimary }
-                        gridColumnStart = { 1 }
-                        gridColumnEnd = { 2 }
-                        gridRowStart = { 1 }
-                        gridRowEnd = { 2 }
+                    <WrapperGridBasket
+                        rows = { 1 }
+                        rowWidth = { 64 }
+                        justifyContent="center"
                     >
-                        Product
-                    </Text>
-                    <Text
-                        fontSize = { 20 }
-                        lineHeight = { 27 }
-                        color = { COLOR.charcoalPrimary }
-                        gridColumnStart = { 2 }
-                        gridColumnEnd = { 3 }
-                        gridRowStart = { 1 }
-                        gridRowEnd = { 2 }
-                    >
-                        Price
-                    </Text>
-                    <Text
-                        fontSize = { 20 }
-                        lineHeight = { 27 }
-                        color = { COLOR.charcoalPrimary }
-                        gridColumnStart = { 3 }
-                        gridColumnEnd = { 4 }
-                        gridRowStart = { 1 }
-                        gridRowEnd = { 2 }
-                    >
-                        Quantity
-                    </Text>
-                    <Text
-                        fontSize = { 20 }
-                        lineHeight = { 27 }
-                        color = { COLOR.charcoalPrimary }
-                        gridColumnStart = { 4 }
-                        gridColumnEnd = { 5 }
-                        gridRowStart = { 1 }
-                        gridRowEnd = { 2 }
-                    >
-                        Total
-                    </Text>
-                    <WrapperFlexWithoutMarginAuto
-                        marginLeft = { 205 }
-                        maxWidth = { 160 }
-                        columnGap = { 20 }
-                        gridColumnStart = { 1 }
-                        gridColumnEnd = { 2 }
-                        gridRowStart = { 2 }
-                        gridRowEnd = { 3 }
-                    >
-                        <StickerSmallWrapper>
-                            <Sticker
-                                src = { "/images/Stickers/GirlSticker.svg" }
-                                alt = "Girl"
-                                maxWidth = { 64 }
-                                maxHeight = { 64 }
-                                borderRadius = { 10 }
-                            />
-                        </StickerSmallWrapper>
                         <Text
                             fontSize = { 20 }
                             lineHeight = { 27 }
+                            color = { COLOR.charcoalPrimary }
+                            gridColumnStart = { 1 }
+                            gridColumnEnd = { 2 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        >
+                            Product
+                        </Text>
+                        <Text
+                            fontSize = { 20 }
+                            lineHeight = { 27 }
+                            color = { COLOR.charcoalPrimary }
+                            gridColumnStart = { 2 }
+                            gridColumnEnd = { 3 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        >
+                            Price
+                        </Text>
+                        <Text
+                            fontSize = { 20 }
+                            lineHeight = { 27 }
+                            color = { COLOR.charcoalPrimary }
+                            gridColumnStart = { 3 }
+                            gridColumnEnd = { 4 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        >
+                            Quantity
+                        </Text>
+                        <Text
+                            fontSize = { 20 }
+                            lineHeight = { 27 }
+                            color = { COLOR.charcoalPrimary }
+                            gridColumnStart = { 4 }
+                            gridColumnEnd = { 5 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        >
+                            Total
+                        </Text>
+                    </WrapperGridBasket>
+                { stickersInBasket.map((sticker: IStickers, index: number, array: IStickers[]) => (
+                    <WrapperGridBasket
+                        rows = { 1 }
+                        rowWidth = { 64 }
+                        justifyContent="center"
+                    >
+                        <WrapperFlexWithoutMarginAuto
+                            marginLeft = { 205 }
+                            maxWidth = { 160 }
+                            columnGap = { 20 }
+                            gridColumnStart = { 1 }
+                            gridColumnEnd = { 2 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        >
+                            <StickerSmallWrapper>
+                                <Sticker
+                                    src = { sticker.src }
+                                    alt = { sticker.name }
+                                    maxWidth = { 64 }
+                                    maxHeight = { 64 }
+                                    borderRadius = { 10 }
+                                />
+                            </StickerSmallWrapper>
+                            <Text
+                                fontSize = { 20 }
+                                lineHeight = { 27 }
+                                fontWeight = { 400 }
+                                color = { COLOR.charcoalPrimary }
+                            >
+                                { sticker.name }
+                            </Text>
+                        </WrapperFlexWithoutMarginAuto>
+                        <Text
+                            fontSize = { 18 }
+                            lineHeight = { 25 }
                             fontWeight = { 400 }
                             color = { COLOR.charcoalPrimary }
+                            gridColumnStart = { 2 }
+                            gridColumnEnd = { 3 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
                         >
-                            Girl
+                            { sticker.price }
                         </Text>
-                    </WrapperFlexWithoutMarginAuto>
-                    <Text
-                        fontSize = { 18 }
-                        lineHeight = { 25 }
-                        fontWeight = { 400 }
-                        color = { COLOR.charcoalPrimary }
-                        gridColumnStart = { 2 }
-                        gridColumnEnd = { 3 }
-                        gridRowStart = { 2 }
-                        gridRowEnd = { 3 }
-                    >
-                        $1.2
-                    </Text>
-                    <CounterButtons
-                        colorText = { COLOR.charcoalPrimary }
-                        backgroundColorText = { COLOR.whitePrimary }
-                        gridColumnStart = { 3 }
-                        gridColumnEnd = { 4 }
-                        gridRowStart = { 2 }
-                        gridRowEnd = { 3 }
-                    />
-                    <Text
-                        fontSize = { 18 }
-                        lineHeight = { 25 }
-                        fontWeight = { 400 }
-                        color = { COLOR.charcoalPrimary }
-                        gridColumnStart = { 4 }
-                        gridColumnEnd = { 5 }
-                        gridRowStart = { 2 }
-                        gridRowEnd = { 3 }
-                    >
-                        $1.2
-                    </Text>
-                    <Cross
-                        marginLeft = { 30 }
-                        width = { 40 }
-                        height = { 40 }
-                        widthSpan = { 24 }
-                        heightSpan = { 2 }
-                        backgroundColorSpan = { COLOR.ashGray }
-                        hoverBackgroundColorSpan = { COLOR.charcoalPrimary }
-                        activeBackgroundColorSpan = { COLOR.razzmatazzPrimary }
-                        gridColumnStart = { 5 }
-                        gridColumnEnd = { 6 }
-                        gridRowStart = { 2 }
-                        gridRowEnd = { 3 }
-                    />
-                </WrapperGridBasket>
+                        <CounterButtons
+                            colorText = { COLOR.charcoalPrimary }
+                            backgroundColorText = { COLOR.whitePrimary }
+                            gridColumnStart = { 3 }
+                            gridColumnEnd = { 4 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        />
+                        <Text
+                            fontSize = { 18 }
+                            lineHeight = { 25 }
+                            fontWeight = { 400 }
+                            color = { COLOR.charcoalPrimary }
+                            gridColumnStart = { 4 }
+                            gridColumnEnd = { 5 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        >
+                            { sticker.price }
+                        </Text>
+                        <Cross
+                            marginLeft = { 30 }
+                            width = { 40 }
+                            height = { 40 }
+                            widthSpan = { 24 }
+                            heightSpan = { 2 }
+                            backgroundColorSpan = { COLOR.ashGray }
+                            hoverBackgroundColorSpan = { COLOR.charcoalPrimary }
+                            activeBackgroundColorSpan = { COLOR.razzmatazzPrimary }
+                            gridColumnStart = { 5 }
+                            gridColumnEnd = { 6 }
+                            gridRowStart = { 1 }
+                            gridRowEnd = { 2 }
+                        />
+                    </WrapperGridBasket>
+                    ))}
                 <CommonButtonMaxWidth
+                    marginTop = { 48 }
                     marginBottom = { 64 }
                     marginLeft = { 825 }
                     paddingTop = { 12 }
