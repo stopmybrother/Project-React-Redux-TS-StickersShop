5878b1e7c9ee9dddba75ccec8ba005c831723ee7
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 21:16:02 +0300

    fix: added props for sorting in containers/Sort/Sort.tsx

--- project-react-ts/src/containers/Sort/Sort.tsx
+++ project-react-ts/src/containers/Sort/Sort.tsx
@@ -3,15 +3,37 @@ import { WrapperFlexWithoutMarginAuto } from "../../styled-components/components
 import { SortTags } from "../../components/SortButtons/SortButtons";
 import { Select } from "../../components/Select/Select";
 
-export const Sort = () => {
+export interface ISortProps {
+    amount?: number;
+    activeFilter?: string;
+    activeTag?: string;
+    filterChange?: ( filter: string ) => void;
+    sortSticker?: ( tag: string ) => void;
+};
+
+export const Sort = ( {
+                          amount,
+                          activeFilter,
+                          activeTag,
+                          filterChange,
+                          sortSticker
+                      }: ISortProps ) => {
     return (
             <WrapperFlexWithoutMarginAuto
                 marginBottom = { 48 }
                 maxWidth = { 1232 }
                 justifyContent = "space-between"
             >
-                <SortTags />
-                <Select />
+                <SortTags
+                    amount = { amount }
+                    activeTag = { activeTag }
+                    sortSticker = { sortSticker }
+                />
+                <Select
+                    amount = { amount }
+                    activeFilter = { activeFilter }
+                    filterChange = { filterChange }
+                />
             </ WrapperFlexWithoutMarginAuto>
     );
 };
