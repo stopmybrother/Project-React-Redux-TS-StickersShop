2e973c435a6aeb8b990dad1483971a3b81649f96
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 20:56:49 +0300

    feature: added stickerReducer in redux/reducers/stickersReducer/stickersReducer.tsx

---
+++ project-react-ts/src/redux/reducers/stickersReducer/stickersReducer.tsx
@@ -0,0 +1,36 @@
+import { IStickers, STICKERS } from "../../../constants/stickers";
+import { TStickersActionTypes } from "../../actions/stickersActionCreactors/stickersActionCreator";
+import { ADD_STICKER, LOVE_STICKER } from "../../actions/actions";
+
+const initialState = STICKERS;
+
+const stickersReducer = (
+                            state: IStickers[] = initialState,
+                            { type, payload }: TStickersActionTypes
+                          ) => {
+    switch ( type ) {
+        case ADD_STICKER:
+            return [
+                ...state, {
+                    id: payload.id,
+                    src: payload.src,
+                    name: payload.name,
+                    category: payload.category,
+                    price: payload.price,
+                    currency: payload.currency,
+                    stockStatus: payload.stockStatus,
+                    isRelevant: payload.isRelevant,
+                    isNew: payload.isNew,
+                    loading: payload.loading,
+                    hovered: payload.hovered,
+                    loved: payload.loved,
+                    added: payload.added,
+                }
+            ];
+        case LOVE_STICKER:
+            return state.filter((sticker) => sticker.id === payload.id)
+        default:
+            return state;
+    };
+};
+export default stickersReducer;
