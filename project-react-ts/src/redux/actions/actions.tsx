39130d5239fcb9f9f431dcf517cb17411ed72715
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 20:52:41 +0300

    feature: added constants for action creators in redux/actions/actions.tsx

---
+++ project-react-ts/src/redux/actions/actions.tsx
@@ -0,0 +1,4 @@
+export const ADD_STICKER = "ADD_STICKER" as const;
+export const LOVE_STICKER = "LOVE_STICKER" as const;
+export const SORT_STICKERS = "CHANGE_FILTER" as const;
+export const CHANGE_FILTER = "CHANGE_FILTER" as const;
