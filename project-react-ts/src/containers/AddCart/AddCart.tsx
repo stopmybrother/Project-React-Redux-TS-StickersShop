b4810a4fbcdea768446a67008ec2d1795af4e771
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 21:11:00 +0300

    fix: added NavLink, added sorted stickers, deleted extra imports and other in containers/AddCart/AddCart.tsx

--- project-react-ts/src/containers/AddCart/AddCart.tsx
+++ project-react-ts/src/containers/AddCart/AddCart.tsx
@@ -15,8 +15,13 @@ import { SortTags } from "../../components/SortButtons/SortButtons";
 import { TextLink } from "../../styled-components/components/Link";
 import { CommonButton } from "../../components/common-components/Button";
 import { Counter } from "../../components/Counter/Counter";
+import { RouterLink } from "../../components/common-components/RouterLink";
+import { STICKERS, IStickers } from "../../constants/stickers";
 
-export const AddCart = () => {
+export const AddCart = ( { addedSticker }: any ) => {
+    STICKERS.map((sticker) => sticker.added = false)
+    console.log(addedSticker)
+    console.log(STICKERS)
     return (
         <>
             <GeneralWrapper
@@ -28,45 +33,46 @@ export const AddCart = () => {
                     paddingRight = { 16 }
                     paddingLeft = { 16 }
                 >
+                    { addedSticker.map( ( sticker: IStickers ) => (
                     <WrapperGridWithoutMarginAuto
                     >
-                        <StickerWrapper>
-                            <Sticker
-                                src ="/images/Stickers/CoffeeSticker.svg"
-                                alt = { "Coffee" }
-                                borderRadius = { 10 }
-                            />
-                        </StickerWrapper>
+                            <StickerWrapper>
+                                <Sticker
+                                    src = { sticker.src }
+                                    alt = { sticker.name }
+                                    borderRadius = { 10 }
+                                />
+                            </StickerWrapper>
                         <WrapperWithoutMarginAuto
                             marginBottom = { 48 }
                             maxWidth = { 211 }
                         >
-                            <Text
-                                marginBottom = { 24 }
-                                fontSize = { 36 }
-                                lineHeight = { 49 }
-                                color = { COLOR.spaceCadetPrimary }
-                                textAlign = "left"
-                            >
-                                Coffee
-                            </Text>
-                            <Text
-                                marginBottom = { 36 }
-                                color = { COLOR.razzmatazzPrimary }
-                                textAlign = "left"
-                            >
-                                $1.5
-                            </Text>
-                            <TextLink
-                                marginBottom = { 33 }
-                                fontSize = { 14 }
-                                lineHeight = { 19 }
-                                fontWeight = { 400 }
-                                color = { COLOR.spaceCadetPrimary }
-                                textAlign = "left"
-                            >
-                                More information about sticker
-                            </TextLink>
+                                <Text
+                                    marginBottom = { 24 }
+                                    fontSize = { 36 }
+                                    lineHeight = { 49 }
+                                    color = { COLOR.spaceCadetPrimary }
+                                    textAlign = "left"
+                                >
+                                    { sticker.name}
+                                </Text>
+                                <Text
+                                    marginBottom = { 36 }
+                                    color = { COLOR.razzmatazzPrimary }
+                                    textAlign = "left"
+                                >
+                                    { sticker.price }{ sticker.currency }
+                                </Text>
+                                <TextLink
+                                    marginBottom = { 33 }
+                                    fontSize = { 14 }
+                                    lineHeight = { 19 }
+                                    fontWeight = { 400 }
+                                    color = { COLOR.spaceCadetPrimary }
+                                    textAlign = "left"
+                                >
+                                    More information about sticker
+                                </TextLink>
                             <Counter
                                 colorText = { COLOR.whiteSecondary }
                                 backgroundColorText = { COLOR.metallicBlue }
@@ -95,11 +101,19 @@ export const AddCart = () => {
                                 activeBackgroundColor = { COLOR.maastrichtBlueSecondary }
                                 activeColor = { COLOR.whitePrimary }
                             >
-                                Add to cart
+                                <RouterLink
+                                    to = "/home"
+                                    color = { COLOR.whitePrimary }
+                                    hoverColor = { COLOR.whitePrimary }
+                                    activeColor = { COLOR.whitePrimary }
+                                >
+                                    Add to cart
+                                </RouterLink>
                             </CommonButton>
                         </WrapperWithoutMarginAuto>
                         <SortTags/>
                     </WrapperGridWithoutMarginAuto>
+                    )) }
                 </Wrapper>
             </GeneralWrapper>
         </>
