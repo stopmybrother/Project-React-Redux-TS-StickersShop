f755b50ce5879b6b6eb85f4389805bd5680b90bf
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-29 23:44:20 +0300

    fix: added REMOVE_STICKER in project-react-ts/src/redux/actions/actions.tsx

--- project-react-ts/src/redux/actions/actions.tsx
+++ project-react-ts/src/redux/actions/actions.tsx
@@ -1,4 +1,5 @@
 export const ADD_STICKER = "ADD_STICKER" as const;
+export const REMOVE_STICKER = "REMOVE_STICKER" as const;
 export const LOVE_STICKER = "LOVE_STICKER" as const;
 export const SORT_STICKERS = "CHANGE_FILTER" as const;
 export const CHANGE_FILTER = "CHANGE_FILTER" as const;
