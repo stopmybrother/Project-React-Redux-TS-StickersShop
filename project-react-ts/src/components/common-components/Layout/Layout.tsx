b7846197f32a1c8b6c21b60d42762582c97d99af
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-31 02:44:51 +0300

    fix: remove ThemeProvider and GlobalStyles in components/common-components/Layout/Layout.tsx

--- project-react-ts/src/components/common-components/Layout/Layout.tsx
+++ project-react-ts/src/components/common-components/Layout/Layout.tsx
@@ -1,50 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { themes } from "../../../styled-components/themes";
 import { Header } from "../../../containers/Header/Header";
 import { Footer } from "../../../containers/Footer/Footer";
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  button {
-    display: block;
-    cursor: pointer;
-    outline: none;
-  }
-
-  a {
-    display: block;
-    cursor: pointer;
-    text-decoration: none;
-  }
-  
-  input {
-    display: block;
-    cursor: pointer;
-    outline: none;
-  }
-  span {
-    display: block;
-  }
-  img {
-    display: block;
-  }
-`;
 
 export const Layout = () => {
     return (
         <>
-            <ThemeProvider theme = { themes }>
-                <GlobalStyle />
-                <Header />
-                    <Outlet />
-                <Footer />
-            </ThemeProvider>
+            <Header />
+                <Outlet />
+            <Footer />
         </>
     );
 };
