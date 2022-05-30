ff385615515bb11de1bcc86359eb3451679c30db
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-31 02:22:44 +0300

    fix: added action for counter in  project-react-ts/src/redux/actions/actions.tsx

--- project-react-ts/src/redux/actions/actions.tsx
+++ project-react-ts/src/redux/actions/actions.tsx
@@ -3,3 +3,6 @@ export const REMOVE_STICKER = "REMOVE_STICKER" as const;
 export const LOVE_STICKER = "LOVE_STICKER" as const;
 export const SORT_STICKERS = "CHANGE_FILTER" as const;
 export const CHANGE_FILTER = "CHANGE_FILTER" as const;
+
+export const INCREMENT = " INCREMENT" as const;
+export const DECREMENT = " DECREMENT" as const;
