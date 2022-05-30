bed6c4ce43dd3fbbea9ecd3c6b0dcfd74e46553e
Zhenya Aleksandrova <laberfo@gmail.com>
2022-05-31 02:29:50 +0300

    fix: replace ThemeProvider and GlobalStyles in index.tsx

--- project-react-ts/src/index.tsx
+++ project-react-ts/src/index.tsx
@@ -5,12 +5,48 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import './index.css';
 import App from "./containers/App/App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import {themes} from "./styled-components/themes";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  button {
+    display: block;
+    cursor: pointer;
+    outline: none;
+  }
+
+  a {
+    display: block;
+    cursor: pointer;
+    text-decoration: none;
+  }
+  
+  input {
+    display: block;
+    cursor: pointer;
+    outline: none;
+  }
+  span {
+    display: block;
+  }
+  img {
+    display: block;
+  }
+`;
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store = { store }>
-            <App />
-        </Provider>
-    </BrowserRouter>,
+    <ThemeProvider theme = { themes }>
+        <BrowserRouter>
+                <Provider store = { store }>
+                    <GlobalStyle />
+                    <App />
+                </Provider>
+        </BrowserRouter>
+    </ThemeProvider>,
     document.getElementById('root')
 );
